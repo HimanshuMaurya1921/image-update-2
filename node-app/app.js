2bb2ab29c34f9e0c2dbcfd031d075f88bd724d74
Himanshu Maurya <himanshumaurya1921@gmail.com>
2026-04-22 16:45:02 +0530

    feat: initial node-app with argocd image updater setup

---
+++ node-app/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node App v1.0.0\n');
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
